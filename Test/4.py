17a66ea96b91361635f787ab78bea5bc0080dba7
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-22 21:02:57 +0600

    provlem one on provlem 2 is solve

--- Test/4.py
+++ Test/4.py
@@ -1,2 +1,45 @@
 #  python3 4.py
-print("hello world")
+print("""
+██████╗ ██╗   ██╗████████╗██╗  ██╗ ██████╗ ███╗   ██╗
+██╔══██╗╚██╗ ██╔╝╚══██╔══╝██║  ██║██╔═══██╗████╗  ██║
+██████╔╝ ╚████╔╝    ██║   ███████║██║   ██║██╔██╗ ██║
+██╔═══╝   ╚██╔╝     ██║   ██╔══██║██║   ██║██║╚██╗██║
+██║        ██║      ██║   ██║  ██║╚██████╔╝██║ ╚████║
+╚═╝        ╚═╝      ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+""")
+
+# problem 1
+
+def calculateTax(income,expenses):
+    if (income < expenses ):
+        return "Invalid Input"
+    elif(income <= 0):
+        return "Invalid Input"
+    elif(expenses <= 0):
+        return "Invalid Input" 
+    def sum(income,expenses):
+        Deferent = income - expenses
+        Tax = Deferent*.20
+        return Tax
+    return sum(income,expenses)
+    
+pro1 = calculateTax( 10000,3000 )
+print(pro1)
+
+# problem 2
+
+def sendNotication(email):
+    if "@" in email :
+        if "." in email :
+            array = email.split("@");
+            name = array[0]
+            web = array[1]
+            data = name + " sent you an email from " + web
+            return data
+        else:
+            return "Invalid Email"
+    else :
+        return "Invalid Email"
+
+pro2 = sendNotication("ash@gmail.com")
+print(pro2)
